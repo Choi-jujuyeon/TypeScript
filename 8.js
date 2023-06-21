d9c0d5120cc89364415d313b80848347176f5010
Choi-jujuyeon <chlwndus1216@naver.com>
2023-06-21 11:13:14 +0900

    Ts-Object (+배열)

--- 8.js
+++ 8.js
@@ -4,3 +4,12 @@ var myInfo = {
     isRich: false,
 };
 console.log(myInfo.name);
+//응용- 배열 사용할 경우
+var myInfo2 = [
+    { name: "Choi", age: 99, isRich: true },
+    { name: "Ju", age: 10, isRich: false },
+    { name: "Yeon", age: 20, isRich: false },
+];
+console.log(myInfo2[0].name);
+console.log(myInfo2[1].age);
+console.log(myInfo2[2].isRich);
--- 8.ts
+++ 8.ts
@@ -5,3 +5,13 @@ let myInfo: { name: string; age: number; isRich: boolean } = {
 };
 
 console.log(myInfo.name);
+
+//응용- 배열 사용할 경우
+let myInfo2: { name: string; age: number; isRich: boolean }[] = [
+    { name: "Choi", age: 99, isRich: true },
+    { name: "Ju", age: 10, isRich: false },
+    { name: "Yeon", age: 20, isRich: false },
+];
+console.log(myInfo2[0].name);
+console.log(myInfo2[1].age);
+console.log(myInfo2[2].isRich);
